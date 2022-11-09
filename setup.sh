5a709d5dc929e13854922908e69c446212e74808
Hudson Smith <76886104+hudsonsmith@users.noreply.github.com>
2022-11-09 14:01:09 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cd /tmp; wget https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb; sudo apt-get install ./nvim-linux64.deb
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
-echo "alias lvim='/home/gitpod/.local/bin/lvim'"
+# echo "alias lvim='/home/gitpod/.local/bin/lvim'"
 
-curl -K https://raw.githubusercontent.com/hudsonsmith/gitpod_template/main/config.lua >> ~/.config/lvim/config.lua
+curl https://raw.githubusercontent.com/hudsonsmith/gitpod_template/main/config.lua > ~/.config/lvim/config.lua
 
 lvim
